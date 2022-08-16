73b7737b7c1f71f30b49daa8c0bdc56fb5860b84
Ed Beard <ed.r.beard@gmail.com>
2022-08-16 11:48:20 -0400

    Random String Generated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-//var length = Number(prompt("How many characters (Minimum of 8)?" ));
-//var specChar = prompt("Would you like Special Characters?");
-//var lcChar = prompt("Would you like lower case letters?");
-//var ucChar = prompt("Would you like upper case letters?");
-//var numbers = prompt("Would you like numbers?")
 
+function randomInt(min,max) {
+  return Math.floor(Math.random()*(max-min)+min)
+}
+
+function getRandom(list) {
+  return list[randomInt(0,list.length)]
+}
 
 function generatePassword() {
   //Propmt for Length of Code (Minimim 8 Characters)
@@ -26,16 +28,17 @@ function generatePassword() {
   var numbersIncl = window.confirm("Would you like numbers in your password?")
   //Prompt for Special Characters
   var specialCharIncl = window.confirm("Would you like special characters in your password?")
-  //Prompt for uppercase Characters
-  var upcaseCharIncl = window.confirm("Would you like uppercase characters in your password?")
   //Prompt for lowercase Characters
   var lowcaseCharIncl = window.confirm("Would you like lowercase characters in your password?")
+  //Prompt for uppercase Characters
+  var upcaseCharIncl = window.confirm("Would you like uppercase characters in your password?")
+
 
   //Selection Lists
   var numberList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
   var symbolList = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+"]
-  var upperCaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-  var lowerCaseLise = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+  var lowerCaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  var upperCaseList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
   var userSelections = []
 
@@ -55,7 +58,18 @@ function generatePassword() {
     userSelections.push(upperCaseList)
   }
 
-console.log(userSelections)
+  if (userSelections === 0) {
+    userSelections.push(upperCaseList)
+  }
+
+  var generatedPassword = ""
+
+for (var i = 0; i < passLength; i++) {
+  var randomList = getRandom(userSelections)
+  var randomChar = getRandom(randomList)
+  console.log(randomChar)
+}
+
 
 }
 
@@ -63,8 +77,6 @@ console.log(userSelections)
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
 
 }
 
